46ce89c130b5e52d5666006574b8df6fee4ce8b8
Nicolosh <nicolo.cavalieri@studenti.unitn.it>
2022-12-24 18:39:36 +0100

    Almost completed necessary 5 steps for each block

--- src/block_la.c
+++ src/block_la.c
@@ -20,11 +20,12 @@
 // |_| \_|\___| \_/\_/   |_|  \___|\__,_|\__|\__,_|_|  \___||___/
 // ============================================================================
 
-//  ____ _____ _____ ____    _ 
+//  ____ _____ _____ ____    _
 // / ___|_   _| ____|  _ \  / |
 // \___ \ | | |  _| | |_) | | |
 //  ___) || | | |___|  __/  | |
 // |____/ |_| |_____|_|     |_|
+// Intermediate velocities
 
 data_t dot_product(const point_t *p1, const point_t *p2, const point_t *p3) {
   assert(p1 && p2 && p3);
@@ -33,8 +34,22 @@ data_t dot_product(const point_t *p1, const point_t *p2, const point_t *p3) {
               (point_z(p2) - point_z(p1)) * (point_z(p3) - point_z(p2)));
 }
 
-data_t cosAlpha(const block_t *b) 
-{
+// ORDERING ELEMENTS IN ASCENDENT ORDER
+void ascending(data_t *vector, data_t size) {
+  assert(vector);
+  int i = 0;
+  while (i < size) {
+    // SWAPPING
+    if (vector[i] > vector[i + 1]) {
+      data_t tmp = vector[i];
+      vector[i] = vector[i + 1];
+      vector[i + 1] = tmp;
+    }
+    i++;
+  }
+}
+
+data_t cosAlpha(const block_t *b) {
   assert(b);
   data_t cos_alpha;
   point_t *p1 = point_zero(b);
@@ -56,7 +71,7 @@ data_t cosAlpha(const block_t *b)
   else if ((block_type(b) == ARC_CW || block_type(b) == ARC_CCW) &&
            block_type(block_next(b)) == LINE) {
     data_t dot = dot_product(center, p1, p2);
-    data_t beta = acos(dot / (v1_arc * v2_lin)); //radiants
+    data_t beta = acos(dot / (v1_arc * v2_lin)); // radiants
     cos_alpha = cos(M_PI / 2.0 - beta);
   }
 
@@ -64,70 +79,130 @@ data_t cosAlpha(const block_t *b)
   else if (block_type(b) == LINE && (block_type(block_next(b)) == ARC_CW ||
                                      block_type(block_next(b)) == ARC_CCW)) {
     data_t dot = dot_product(p1, p2, center_next);
-    data_t beta = acos(dot / (v1_lin * v2_arc)); //radiants
+    data_t beta = acos(dot / (v1_lin * v2_arc)); // radiants
     cos_alpha = cos(M_PI / 2.0 - beta);
   }
 
   // all 4 combinations of ARC_CW and ARC_CCW blocks
-  else
-  {
+  else {
     data_t dot = dot_product(center, p2, center_next);
     cos_alpha = dot / (v1_arc * v2_arc);
   }
   return cos_alpha;
 }
 
-data_t maintenanceVel(const block_t *b)
-{
+data_t maintenanceVel(const block_t *b) {
   assert(b);
   data_t vm;
-  
+
   // LINEAR block
-  if(block_type(b) == LINE)
-    vm = b->feedrate;
-  
+  if (block_type(b) == LINE)
+    vm = b->feedrate; // [mm/min]
+
   // CLOCK or COUNTERCLOCKWISE block
-  else if(block_type(b) == ARC_CW || block_type(b) == ARC_CCW)
+  else if (block_type(b) == ARC_CW || block_type(b) == ARC_CCW)
     vm = sqrt(machine_A(b->machine) * block_r(b)) * 60; // [mm/min]
 
   return vm;
 }
 
-data_t finalVel(const block_t *b)
-{
+data_t finalVel(const block_t *b) {
   assert(b);
-  data_t vm, vm_next, cos_alpha;
-  
-  vm = maintenanceVel(b);
-  vm_next = maintenanceVel(block_next(b));
-  cos_alpha = cosAlpha(b);
+
+  data_t vm = maintenanceVel(b);
+  data_t vm_next = maintenanceVel(block_next(b));
+  data_t cos_alpha = cosAlpha(b);
+
+  if (cos_alpha > (sqrt(2) / 2)) // angle > 45° ==> 0 speed
+    return 0.0;
 
   return (vm + vm_next) / 2.0 * cos_alpha;
 }
 
-data_t initialVel(const block_t *b)
-{
+data_t initialVel(const block_t *b) {
   assert(b);
   data_t vi;
-  if(b->prev)
+  if (b->prev)
     vi = finalVel(b->prev);
 
   return vi;
 }
 
-//  ____ _____ _____ ____    ____  
-// / ___|_   _| ____|  _ \  |___ \ 
-// \___ \ | | |  _| | |_) |   __) |
-//  ___) || | | |___|  __/   / __/ 
-// |____/ |_| |_____|_|     |_____|
-                                 
+void settingVel_sisf(const block_t *b) {
+  assert(b);
+  b->prof->initialVel = initialVel(b);
+  b->prof->manintenanceVel = maintenanceVel(b);
+  b->prof->finalVel = finalVel(b);
+  
+  // si
+  b->prof->s[0] = 0.0;
+  // sf
+  b->prof->s[S_LENGTH - 1] = block_length(b);
+}
 
-//  ____ _____ _____ ____    _____ 
-// / ___|_   _| ____|  _ \  |___ / 
-// \___ \ | | |  _| | |_) |   |_ \ 
+//  ____ _____ _____ ____    ____      _____
+// / ___|_   _| ____|  _ \  |___ \    |___ /
+// \___ \ | | |  _| | |_) |   __) |____ |_ \
+//  ___) || | | |___|  __/   / __/_____|__) |
+// |____/ |_| |_____|_|     |_____|   |____/
+
+// ============================= Accelerations =================================
+void s1s2_Acc(const block_t *b, data_t MAX_acc, data_t vi, data_t vm, data_t vf,
+              data_t si, data_t sf) {
+  if (vi <= vm)
+    b->prof->s[1] = si + (pow(vm, 2) - pow(vi, 2)) / (2.0 * MAX_acc);
+
+  if (vm <= vf)
+    b->prof->s[2] = sf + (pow(vm, 2) - pow(vf, 2)) / (2.0 * MAX_acc);
+  else
+    s1s2_Dec(b, MAX_acc, vi, vm, vf, si, sf);
+}
+
+// ============================= Decelerations =================================
+void s1s2_Dec(const block_t *b, data_t MAX_acc, data_t vi, data_t vm, data_t vf,
+              data_t si, data_t sf) {
+  if (vi > vm)
+    b->prof->s[1] = si + (pow(vi, 2) - pow(vm, 2)) / (2.0 * MAX_acc);
+
+  if (vm > vf)
+    b->prof->s[2] = sf + (pow(vf, 2) - pow(vm, 2)) / (2.0 * MAX_acc);
+  else
+    s1s2_Acc(b, MAX_acc, vi, vm, vf, si, sf);
+}
+
+//  ____ _____ _____ ____    _  _
+// / ___|_   _| ____|  _ \  | || |
+// \___ \ | | |  _| | |_) | | || |_
+//  ___) || | | |___|  __/  |__   _|
+// |____/ |_| |_____|_|        |_|
+// Calculate the timings
+void timings(const block_t *b, data_t MAX_acc, data_t vi, data_t vm, data_t vf,
+              data_t si, data_t s1, data_t s2, data_t sf) 
+{ 
+  assert(b);
+  // when we accelerate
+  b->prof->t1 = (1 / MAX_acc) * (-vi + sqrt(2 * MAX_acc * (s1 - si) + pow(vi, 2)));
+  b->prof->t2 = (1 / MAX_acc) * (-vm + sqrt(2 * MAX_acc * (s2 - s1) + pow(vm, 2)));
+  b->prof->tf = (1 / MAX_acc) * (-vm + sqrt(2 * MAX_acc * (sf - s2) + pow(vm, 2)));
+  // :......
+}
+//  ____ _____ _____ ____    ____
+// / ___|_   _| ____|  _ \  | ___|
+// \___ \ | | |  _| | |_) | |___ \
 //  ___) || | | |___|  __/   ___) |
-// |____/ |_| |_____|_|     |____/ 
-                                 
+// |____/ |_| |_____|_|     |____/
+// Reshaping
+void reshaping(const block_t *b, data_t tf, data_t tq)
+{
+  assert(b);
+  data_t tf_star = ((size_t)(tf / tq) + 1) * tq;
+  data_t k = tf_star / tf;
+
+  // rescaling velocities
+  b->prof->initialVel /= k;
+  b->prof->maintenanceVel /= k;
+  b->prof->finalVel /= k;
+}
 
 //   _____ _____ ____ _____   __  __       _
 //  |_   _| ____/ ___|_   _| |  \/  | __ _(_)_ __
--- src/block_la.h
+++ src/block_la.h
@@ -15,6 +15,8 @@
 #ifndef BLOCK_LA_H
 #define BLOCK_LA_H
 
+#define S_LENGTH 4
+
 #include "defines.h"
 #include "block.h"
 
@@ -22,6 +24,9 @@
 
 // Scalar Product between 2 CONSECUTIVE vectors given the respective initial and final point for each vector
 data_t dot_product(const point_t *p1, const point_t *p2, const point_t *p3);
+
+//ordering a vector of numbers
+void ascending(data_t *vector, data_t size);
 
 // Compute the cosine of the angle between each pair of consecutive blocks 
 data_t cosAlpha(const block_t *b);
@@ -35,5 +40,20 @@ data_t maintenanceVel(const block_t *b);
 // Final velocity of current block
 data_t finalVel(const block_t *b);
 
+// setting vi, vm and vf for current block and s0 and sf
+void settingVel_sisf(const block_t *b);
+
+// ===================== calculating the 4 notable points ======================
+void s1s2_Acc(const block_t *b, data_t MAX_acc, data_t vi, data_t vm, data_t vf, data_t si, data_t sf);
+
+void s1s2_Dec(const block_t *b, data_t MAX_acc, data_t vi, data_t vm, data_t vf, data_t si, data_t sf);
+// =============================================================================
+
+// calculate timings t1, t2, tf for ranges [si, s1], [s1, s2], [s2, sf]
+void timings(const block_t *b, data_t MAX_acc, data_t vi, data_t vm, data_t vf,
+              data_t si, data_t s1, data_t s2, data_t sf);
+
+// compute k and the rescale velocities
+void reshaping(const block_t *b, data_t tf, data_t tq);
 
 #endif // BLOCK_LA_H
