93c92e7109a79972e296a97cfa1c5a91ad8b65ce
Nicolosh <nicolo.cavalieri@studenti.unitn.it>
2023-02-13 16:21:32 +0100

    Drying code

--- src/program_la.c
+++ src/program_la.c
@@ -11,6 +11,9 @@
 //  |_____\___/ \___/|_|\_\     \__,_|_| |_|\___|\__,_|\__,_|
 
 #include "program_la.h"
+#include "block.h"
+#include "block_la.h"
+#include "program.h"
 
 //  _   _                 _____          _
 // | \ | | _____      __ |  ___|__  __ _| |_ _   _ _ __ ___  ___
@@ -23,95 +26,45 @@
 
 void guards_G00(const program_t *p) {
   assert(p);
-  block_t *b, *iterator = program_first(p);
-  if (block_type(iterator) == RAPID) {
-    b = block_next(iterator);
-    while (b) 
-    {
-      if (block_type(b) == RAPID) {
-        iterator->prof->initialVel = 0.0;
-        b->prof->finalVel = 0.0;
-        iterator = b;
-      }
-      if (maintenanceVel(b) == 0.0)
-        b->prof->finalVel = 0.0;
-      b = block_next(b);
-    } // b = NULL, iterator = last block of program
+  block_t *b = program_first(p);
+
+  while (b) {
+    setFeed_sisf(b);
+    b = block_next(b);
   }
 }
 
-void program_LA_execution(const program_t *p, machine_t *cfg) 
-{
+void program_LA_execution(const program_t *p, machine_t *cfg) {
   assert(p && cfg);
   block_t *b = program_first(p);
-  data_t MAX_acc = machine_A(cfg) * 3600; // to get [mm/min^2]
+  data_t MAX_acc = machine_A(cfg);
   data_t tq = machine_tq(cfg);
-  
-  // setting initial and final velocities for the whole program p
+
+  // setting the known velocities
   guards_G00(p);
 
-  while(b)
-  {
-    // setting intermediate velocities and initial and final s coordinate for each block b
-    settingVel_sisf(b);
+  while (b) {
+    data_t vi = block_FS(b);
+    data_t vm = block_FM(b);
+    data_t vf = block_FE(b);
+    data_t si = block_si(b);
+    data_t sf = block_sf(b);
+    data_t v_star = block_v_star(b);
 
-    data_t vi = b->prof->initialVel;
-    data_t vf = b->prof->finalVel;
-    data_t vm = b->prof->maintenanceVel;
-    data_t si = b->prof->s[0];
-    data_t sf = b->prof->s[S_LENGTH-1];
-  
-    // computation of s1 and s2
-    if(vi <= vm)
-      s1s2_Acc(b, MAX_acc, vi, vm, vf, si, sf);
-    else
-      s1s2_Dec(b, MAX_acc, vi, vm, vf, si, sf);
+    // computation of s1 and s2 using Forward and Backward step
+    forwardAcc(b, MAX_acc, vi, vm, vf, si, sf);
+    backwardDec(b, MAX_acc, vi, vm, vf, si, sf);
 
-    // reordering the s coordinates for each and every block
-    ascending(b->prof->s, S_LENGTH);
+    // so Now I need to recalculate s1, s2 for each block because can happen
+    // that s2 < s1 (WRONG)
+    recompute_s1s2(b, MAX_acc, vi, v_star, vf, si, sf);
 
     // change variable from curvilinear abscissa (s) to time (t)
-    timings(b, MAX_acc, vi, vm, vf, si, b->prof->s[1], b->prof->s[2], sf);
+    timings(b, MAX_acc, vi, v_star, vf, block_s1(b), block_s2(b));
 
-    reshaping(b, b->prof->tf, tq);
-
-    // so Now I recalculate the accelerations and decelerations for each block
-    // until now we use the nominal max acceleration (also equal in magnitude to the max deceleration)
-    if(vi <= vm)
-      s1s2_Acc(b, MAX_acc, vi, vm, vf, si, sf);
-    else
-      s1s2_Dec(b, MAX_acc, vi, vm, vf, si, sf);
+    // reshaping for D.T.
+    reshape(b);
 
     b = block_next(b);
   }
-  
 }
-
-//   _____ _____ ____ _____   __  __       _
-//  |_   _| ____/ ___|_   _| |  \/  | __ _(_)_ __
-//    | | |  _| \___ \ | |   | |\/| |/ _` | | '_ \
-//    | | | |___ ___) || |   | |  | | (_| | | | | |
-//    |_| |_____|____/ |_|   |_|  |_|\__,_|_|_| |_|
-//
-#ifdef PROGRAM_LA_MAIN
-int main() {
-  block_t *b1 = NULL, *b2 = NULL, *b3 = NULL;
-  machine_t *cfg = machine_new(NULL);
-
-  b1 = block_new("N10 G00 X90 Y90 Z100 t3", NULL, cfg);
-  block_parse(b1);
-  b2 = block_new("N20 G01 Y100 X100 F1000 S2000", b1, cfg);
-  block_parse(b2);
-  b3 = block_new("N30 G01 Y200", b2, cfg);
-  block_parse(b3);
-
-  block_print(b1, stdout);
-  block_print(b2, stdout);
-  block_print(b3, stdout);
-
-  block_free(b1);
-  block_free(b2);
-  block_free(b3);
-  return 0;
-}
-#endif
--- src/program_la.h
+++ src/program_la.h
@@ -1,10 +1,10 @@
-//   ____                                      
-//  |  _ \ _ __ ___   __ _ _ __ __ _ _ __ ___  
+//   ____
+//  |  _ \ _ __ ___   __ _ _ __ __ _ _ __ ___
 //  | |_) | '__/ _ \ / _` | '__/ _` | '_ ` _ \
 //  |  __/| | | (_) | (_| | | | (_| | | | | | |
 //  |_|   |_|  \___/ \__, |_|  \__,_|_| |_| |_|
-//                   |___/                     
-//   _                _               _                    _ 
+//                   |___/
+//   _                _               _                    _
 //  | |    ___   ___ | | __      __ _| |__   ___  __ _  __| |
 //  | |   / _ \ / _ \| |/ /____ / _` | '_ \ / _ \/ _` |/ _` |
 //  | |__| (_) | (_) |   <_____| (_| | | | |  __/ (_| | (_| |
@@ -15,18 +15,16 @@
 #ifndef PROGRAM_LA_H
 #define PROGRAM_LA_H
 
+#include "block_la.h"
 #include "defines.h"
 #include "program.h"
-#include "block_la.h"
 
 // * For example:
 // * int program_look_ahead(program_t *p);
 
-// check for G00 consecutive blocks and set speed to 0
+// check for G00 blocks and set speeds to 0.0 where needed
 void guards_G00(const program_t *p);
 
 void program_LA_execution(const program_t *p, machine_t *cfg);
-
-
 
 #endif // PROGRAM_LA_H
