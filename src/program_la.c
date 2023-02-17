c52019805799425a06008f50e5f95234926eddc5
Nicolosh <nicolo.cavalieri@studenti.unitn.it>
2023-02-17 21:42:02 +0100

    Finished program_la.c functions to use in the main function program_LA_execution

--- src/program_la.c
+++ src/program_la.c
@@ -11,9 +11,6 @@
 //  |_____\___/ \___/|_|\_\     \__,_|_| |_|\___|\__,_|\__,_|
 
 #include "program_la.h"
-#include "block.h"
-#include "block_la.h"
-#include "program.h"
 
 //  _   _                 _____          _
 // | \ | | _____      __ |  ___|__  __ _| |_ _   _ _ __ ___  ___
@@ -28,43 +25,203 @@ void guards_G00(const program_t *p) {
   assert(p);
   block_t *b = program_first(p);
 
-  while (b) {
-    setFeed_sisf(b);
+  while (b && (block_type(b) > RAPID && block_type(b) < NO_MOTION)) {
+    setKnownFeed(b);
+    set_sisf(b);
     b = block_next(b);
+  }
+}
+
+void s1s2_ACC(const program_t *p, data_t MAX_acc, data_t si) {
+  assert(p);
+
+  block_t *b = program_first(p);
+
+  // calculating s1 and s2 with the constraint of only ACCELERATION
+  while (b && (block_type(b) > RAPID && block_type(b) < NO_MOTION)) {
+    forwardAcc_s1s2(b, MAX_acc, block_F(b), si, block_sf(b));
+    b = block_next(b);
+  }
+}
+
+void s1s2_DEC(const program_t *p, data_t MAX_acc, data_t si) {
+  assert(p);
+
+  block_t *b = program_last(p);
+
+  // calculating s1 and s2 with the constraint of only DECELERATION
+  while (b && (block_type(b) > RAPID && block_type(b) < NO_MOTION)) {
+    backwardDec_s1s2(b, MAX_acc, block_F(b), si, block_sf(b));
+    b = block_prev(b);
+  }
+}
+
+void s1s2_ordering(const program_t *p, data_t MAX_acc, data_t si) {
+  assert(p);
+
+  block_t *b = program_first(p);
+
+  while (b && (block_type(b) > RAPID && block_type(b) < NO_MOTION)) {
+    recompute_s1s2(b, MAX_acc, block_FS(b), block_F(b), block_FE(b), si,
+                   block_s1(b), block_s2(b), block_sf(b));
+    b = block_next(b);
+  }
+}
+
+void feed_ACC(const program_t *p, data_t MAX_acc, data_t si) {
+  assert(p);
+
+  block_t *b = program_first(p);
+
+  // calculating admissible velocities with the constraint of only ACCELERATION
+  while (b && (block_type(b) > RAPID && block_type(b) < NO_MOTION)) {
+    recompute_feed_ACC(b, MAX_acc, si, block_sf(b));
+    b = block_next(b);
+  }
+}
+
+void feed_DEC(const program_t *p, data_t MAX_acc, data_t si) {
+  assert(p);
+
+  block_t *b = program_last(p);
+
+  // calculating admissible velocities with the constraint of only DECELERATION
+  while (b && (block_type(b) > RAPID && block_type(b) < NO_MOTION)) {
+    recompute_feed_DEC(b, MAX_acc, si, block_sf(b));
+    b = block_prev(b);
+  }
+}
+
+void timer_LA(const program_t *p, data_t MAX_acc) {
+  assert(p);
+
+  block_t *b = program_first(p);
+
+  while (b && (block_type(b) > RAPID && block_type(b) < NO_MOTION)) {
+    timings(b, MAX_acc, block_FS(b), block_F(b), block_FE(b), block_s1(b),
+            block_s2(b));
+    b = block_next(b);
+  }
+}
+
+void set_path_name(const program_t *p, data_t si) {
+  assert(p);
+
+  block_t *b = program_first(p);
+
+  while (b && (block_type(b) > RAPID && block_type(b) < NO_MOTION)) {
+    path_name(b, block_FS(b), block_F(b), block_FE(b), si, block_s1(b),
+              block_s2(b), block_sf(b));
+    b = block_next(b);
+  }
+}
+
+void correct_ACC_DEC(const program_t *p) {
+  assert(p);
+
+  block_t *b = program_first(p);
+
+  while (b && (block_type(b) > RAPID && block_type(b) < NO_MOTION)) {
+    reshapeAccDec(b, block_FS(b), block_F(b), block_FE(b));
+    b = block_next(b);
+  }
+}
+
+void totalTime_G00_G00(const program_t *p) {
+  assert(p);
+  // start from second block: the first one will be RAPID in the gcode provided
+  block_t *b = program_first(p);
+  data_t TOT = 0.0;
+  int nG00 = 0; // takes into account the number of blocks between 2 G00 blocks
+  int idx = 0;
+
+  while (idx < program_length(p) - 1) {
+    // next block is a RAPID or block_actFeed(b) = 0 or block_next(b) = NULL
+    // or Alpha > 45°
+    if (block_FE(b) == 0.0) {
+      TOT += block_dt(b);
+      nG00++;
+      // Loop for reshaping the velocities of blocks between 2 G00 blocks
+      for (int j = 1; j < nG00; j++) {
+        reshapeFeed(b, block_FS(b), block_F(b), block_FE(b), TOT, 0);
+        b = block_prev(b);
+      }
+      reshapeFeed(b, block_FS(b), block_F(b), block_FE(b), TOT, 1);
+      idx++;
+    } else if (block_type(b) == RAPID) {
+      b = block_next(b);
+      idx++;
+    }
+    // neither RAPID nor the next block is a RAPID
+    else {
+      TOT += block_dt(b);
+      b = block_next(b);
+      nG00++;
+      idx++;
+    }
   }
 }
 
 void program_LA_execution(const program_t *p, machine_t *cfg) {
   assert(p && cfg);
-  block_t *b = program_first(p);
-  data_t MAX_acc = machine_A(cfg);
-  data_t tq = machine_tq(cfg);
+  // Constant Parameters for every block in program p
+  data_t si = block_si(program_first(p));
+  data_t MAX_acc = machine_A(cfg) * 3600; // [mm/min^2]
 
-  // setting the known velocities
+  // ============================== 1° Step ==================================
+  // ***          Setting the known velocities and spaces                  ***
   guards_G00(p);
 
-  while (b) {
-    data_t vi = block_FS(b);
-    data_t vm = block_FM(b);
-    data_t vf = block_FE(b);
-    data_t si = block_si(b);
-    data_t sf = block_sf(b);
-    data_t v_star = block_v_star(b);
+  // ========================== 2° and 3° Step =============================
+  // ***     Forward and Backward calculations (4 notable points)        ***
 
-    // computation of s1 and s2 using Forward and Backward step
-    forwardAcc(b, MAX_acc, vi, vm, vf, si, sf);
-    backwardDec(b, MAX_acc, vi, vm, vf, si, sf);
+  // *** Recomputation of admissible velocities (unfeasible final or initial
+  // velocities) and then of s1, s2 for each block ***
+  feed_ACC(p, MAX_acc, si);
+  //                  ||
+  //                  ||
+  //                  \/
+  // ----- Possible change of fs, fe -----
+  feed_DEC(p, MAX_acc, si);
+  //                  ||
+  //                  ||
+  //                  \/
+  // ----- Possible change of fs, fe -----
+  s1s2_ACC(p, MAX_acc, si);
+  //                  ||
+  //                  ||
+  //                  \/
+  // -----      Change of s1, s2     -----
 
-    // so Now I need to recalculate s1, s2 for each block because can happen
-    // that s2 < s1 (WRONG)
-    recompute_s1s2(b, MAX_acc, vi, v_star, vf, si, sf);
+  s1s2_DEC(p, MAX_acc, si);
+  //                  ||
+  //                  ||
+  //                  \/
+  // -----      Change of s1, s2     -----
 
-    // change variable from curvilinear abscissa (s) to time (t)
-    timings(b, MAX_acc, vi, v_star, vf, block_s1(b), block_s2(b));
+  // ------------------    Can happen that s2 <= s1 (WRONG)   ----------------
+  s1s2_ordering(p, MAX_acc, si);
+  //                  ||
+  //                  ||
+  //                  \/
+  // ----- Possible change of s1 and s2 and it is set a value for v_star -----
+  //                         (if it's A-D or  D-A)                       -----
 
-    // reshaping for D.T.
-    reshape(b);
+  // *** Set the path name for each and every block ***
+  set_path_name(p, si);
 
-    b = block_next(b);
-  }
+  // ========================== 4° Step ====================================
+  // *** Change variable from curvilinear abscissa (s) to time (t) ***
+  timer_LA(p, MAX_acc);
+
+  // ========================== 5° Step ====================================
+  // ***                      Reshaping for D.T.                         ***
+  totalTime_G00_G00(p);
+  //                                   ||
+  //                                   ||
+  //                                   \/
+  // ----- Change of fs, fm, fe and of v_star (if it's A-D or D-A) -----
+
+  // set rescaled acceleration and decelaration
+  correct_ACC_DEC(p);
 }
--- src/program_la.h
+++ src/program_la.h
@@ -25,6 +25,33 @@
 // check for G00 blocks and set speeds to 0.0 where needed
 void guards_G00(const program_t *p);
 
+// compute s1, s2 when only accelerating
+void s1s2_ACC(const program_t *p, data_t MAX_acc, data_t si);
+
+// compute s1, s2 when only decelerating
+void s1s2_DEC(const program_t *p, data_t MAX_acc, data_t si);
+
+// compute fs, fe when only accelerating
+void feed_ACC(const program_t *p, data_t MAX_acc, data_t si);
+
+// compute fs, fe when only decelerating
+void feed_DEC(const program_t *p, data_t MAX_acc, data_t si);
+
+// compute s1, s2 in case A-D, D-A, A and D (also if fm not reached)
+void s1s2_ordering(const program_t *p, data_t MAX_acc, data_t si);
+
+// set the path name for all the blocks in program
+void set_path_name(const program_t *p, data_t si);
+
+// timer of whole program
+void timer_LA(const program_t *p, data_t MAX_acc);
+
+// compute the total time between 2 rapid blocks and do the reshaping of speeds
+void totalTime_G00_G00(const program_t *p);
+
+// set the new acceleration / deceleration due to velocities reshaping
+void correct_ACC_DEC(const program_t *p);
+
 void program_LA_execution(const program_t *p, machine_t *cfg);
 
 #endif // PROGRAM_LA_H
